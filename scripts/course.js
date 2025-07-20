72baae2690e9632dbb165a23535a1ff2b69d9958
clarkedav <desumei@byupathway.edu>
2025-07-20 11:20:31 -0700

    more changes

--- scripts/course.js
+++ scripts/course.js
@@ -3,7 +3,8 @@ const courses = [
     { code: "WDD231", name: "Frontend Development I", credits: 2, completed: false },
     { code: "CSE110", name: "Introduction to Programming", credits: 2, completed: true },
     { code: "CSE210", name: "Programming with Classes", credits: 2, completed: false },
-    { code: "WDD131", name: "Dynamic Wed Fundamentals", credits: 2, completed: false }
+    { code: "WDD131", name: "Dynamic Wed Fundamentals", credits: 2, completed: true },
+    { code: "CSE111", name: "Programming with Functions", credits: 2, completed: true }
 ];
 
 const courseContainer = document.getElementById("courses-container");
@@ -16,7 +17,7 @@ function renderCourses(courseList) {
         const card = document.createElement("div");
         card.classList.add("course");
         if (course.completed) card.classList.add("completed");
-        card.innerHTML = `<strong>${course.code}</strong>: ${course.name} (${course.credits} credits)`;
+        card.innerHTML = `<strong>${course.code}</strong>`;
         courseContainer.appendChild(card);
         credits += course.credits;
     });
